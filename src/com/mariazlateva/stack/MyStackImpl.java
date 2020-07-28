aad38b9148cfd1344f0e50a898f6b4642f730099
MariyaZlateva <mimitoooo@abv.bg>
2020-07-28 19:41:18 +0300

    Added MyStackImpl class.

---
+++ src/com/mariazlateva/stack/MyStackImpl.java
@@ -0,0 +1,66 @@
+package com.mariazlateva.stack;
+
+import java.util.EmptyStackException;
+
+public class MyStackImpl implements MyStack {
+
+    //== fields ==
+
+    private int[] stack;
+    private int top;
+
+    //== constructors ==
+    public MyStackImpl(int capacity) {
+        if (capacity <= 0) {
+            this.stack = new int[10];
+        } else {
+            this.stack = new int[capacity];
+        }
+        this.top = 0;
+
+    }//end of constructor
+
+    // == public methods ==
+    @Override
+    public void push(int value) {
+        if (this.top == this.stack.length) {
+            int[] replacingStack = new int[2 * this.stack.length];
+            System.arraycopy(this.stack, 0, replacingStack, 0, this.top);
+            // it makes the same thing like the System.arraycopy
+//            for (int i = 0, j = 0; i < this.top; i++, j++) {
+//                replacingStack[j] = this.stack[i];
+//            }
+            this.stack = replacingStack;
+        }
+        this.stack[this.top++] = value;
+    }//end of method push
+
+    @Override
+    public int pop() {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        int poppedValue = this.stack[this.top - 1];
+        this.stack[this.top - 1] = 0;
+        this.top--;
+        return poppedValue;
+    }//end of method pop
+
+    @Override
+    public int peek() {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return this.stack[this.top - 1];
+    }//end of peek method
+
+    @Override
+    public int size() {
+        return this.top;
+    }//end of method size
+
+    @Override
+    public boolean isEmpty() {
+        return this.top == 0;
+    }// end of method isEmpty
+}
