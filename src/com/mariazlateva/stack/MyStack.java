c1760ce72d563ba623b62bec060dbef406370c4d
MariyaZlateva <mimitoooo@abv.bg>
2020-07-28 19:03:41 +0300

    Added stack.MyStack interface.

---
+++ src/com/mariazlateva/stack/MyStack.java
@@ -0,0 +1,17 @@
+package com.mariazlateva.stack;
+
+public interface MyStack {
+
+    void push(int value);
+
+    int pop();
+
+    int peek();
+
+    int size();
+
+    boolean isEmpty();
+
+
+
+}// end of interface
